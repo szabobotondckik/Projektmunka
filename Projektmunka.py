3da83886fc435d9d6b19db14e5bb72c98665cc55
kiraly.oliver <kiraly.oliver@ckik.hu>
2024-04-19 13:38:50 +0200

    +orszagberlet, megszamolas, iras elkezdese

--- Projektmunka.py
+++ Projektmunka.py
@@ -1,55 +1,61 @@
-def feladat_eldontes(Vonat_nevek, Indulasok_1, Indulasok_2):
+def feladat_eldontes(Vonat_nevek, Indulasok_1, Indulasok_2, Orszag_berlet):
         feladat = input("Mi legyen a program feladata: ")
+        melyik_fajl = input("Melyik fájlt dolgozzuk fel: ")
         if feladat == "Olvasas" or "olvasas":
             try:
-                melyik_fajl = input("Melyik fájlt dolgozzuk fel: ")
-                if melyik_fajl == "S50 - 303.txt":
-                    befajl_1(Vonat_nevek, Indulasok_1, Indulasok_2)
-                elif melyik_fajl == "Vonatok de orzság bérlet.txt":
-                    befajl_2(Vonat_nevek, Indulasok_1, Indulasok_2)
+                if melyik_fajl == "Be1.txt":
+                    befajl_1(Vonat_nevek, Indulasok_1, Indulasok_2, Orszag_berlet)
+                elif melyik_fajl == "Be2.txt":
+                    befajl_2(Vonat_nevek, Indulasok_1, Indulasok_2, Orszag_berlet)
             except:
                 print("NUH UH")
         if feladat == "Iras" or "iras":
             try:
-                melyik_fajl = input("Melyik fájlt dolgozzuk fel: ")
-                if melyik_fajl == "S50 - 303.txt":
-                    iras_fajl()
-                elif melyik_fajl == "Vonatok de orzság bérlet.txt":
-                    iras_fajl()
+                if melyik_fajl == "Be1.txt":
+                    iras_fajl(Vonat_nevek, Indulasok_1, Indulasok_2, Orszag_berlet)
+                elif melyik_fajl == "Be2.txt":
+                    iras_fajl(Vonat_nevek, Indulasok_1, Indulasok_2, Orszag_berlet)
             except:
                 print("NUH UH")
 
 
-def iras_fajl():
-    ...
+def iras_fajl(Vonat_nevek, Indulasok_1, Indulasok_2, Orszag_berlet):
+    Menetrend_iro = open("Be1.txt", "a", encoding="UTF-8")
+    for sor in Menetrend_iro.readlines():
+        m = sor.split("-")
+        Vonat_nevek.append(m[0])
+        Indulasok_1.append(m[int(1)].strip("\n"))
+        Indulasok_2.append(m[int(2)].strip("\n"))
+    print(Vonat_nevek)
 
-
-def befajl_1(Vonat_nevek, Indulasok_1, Indulasok_2):
-    Menetrend_beolvaso = open("S50 - 303.txt", "r", encoding="UTF-8")
+def befajl_1(Vonat_nevek, Indulasok_1, Indulasok_2, Orszag_berlet):
+    Menetrend_beolvaso = open("Be1.txt", "r", encoding="UTF-8")
     for sor in Menetrend_beolvaso.readlines():
         m = sor.split("-")
         Vonat_nevek.append(m[0])
         Indulasok_1.append(m[int(1)].strip("\n"))
         Indulasok_2.append(m[int(2)].strip("\n"))
+        Orszag_berlet.append(m[int(3)].strip("\n"))
     # print(Vonat_nevek)
     # print(Indulasok)
     Menetrend_beolvaso.close()
 
 
-def befajl_2(Vonat_nevek, Indulasok_1, Indulasok_2):
-    Menetrend_beolvaso = open("Vonatok de orzság bérlet.txt", "r", encoding="UTF-8")
+def befajl_2(Vonat_nevek, Indulasok_1, Indulasok_2, Orszag_berlet):
+    Menetrend_beolvaso = open("Be2.txt", "r", encoding="UTF-8")
     for sor in Menetrend_beolvaso.readlines():
         m = sor.split("-")
         Vonat_nevek.append(m[0])
         Indulasok_1.append(m[int(1)].strip("\n"))
         Indulasok_2.append(m[int(2)].strip("\n"))
+        Orszag_berlet.append(m[int(3)].strip("\n"))
     # print(Vonat_nevek)
     print(Indulasok_2)
     print(Indulasok_1)
     Menetrend_beolvaso.close()
 
 
-def listazas(Vonat_nevek, Indulasok_1, Indulasok_2):
+def listazas(Vonat_nevek, Indulasok_1, Indulasok_2, Orszag_berlet):
     print("Vonatok Nevei:")
     for i in range(len(Vonat_nevek)):
         print(Vonat_nevek[i], end=" ")
@@ -57,10 +63,14 @@ def listazas(Vonat_nevek, Indulasok_1, Indulasok_2):
     print("Vonat Órai indulása:")
     for i in range(len(Indulasok_1)):
         print(Indulasok_1[i], end=" ")
-    print("")
+    print()
     print("Vonat Perci indulása:")
     for i in range(len(Indulasok_2)):
         print(Indulasok_2[i], end=" ")
+    print()
+    for i in range(len(Orszag_berlet)):
+        print(Orszag_berlet[i], end=" ")
+ 
 
 
 
@@ -71,13 +81,13 @@ def gyakorisag_mero(Vonat_nevek):
             maxi = i
             print(f"A leggyakoribb vonat: {Vonat_nevek[i]}")
 
-# def megszamolas(Orszag_berlet):
-#     db = 0
-#     n = len(Orszag_berlet)
-#     for i in range(n):
-#         if Orszag_berlet[i] == "Utazható országbérlettel":
-#             db += 1
-#     return db
+def megszamolas(Orszag_berlet):
+    db = 0
+    n = len(Orszag_berlet)
+    for i in range(n):
+        if Orszag_berlet[i] == "Utazható Országbérlettel":
+            db += 1
+    return db
 
 
 
@@ -86,10 +96,10 @@ def main():
     Indulasok_1 = []
     Indulasok_2 = []
     Orszag_berlet = []
-    feladat_eldontes(Vonat_nevek, Indulasok_1, Indulasok_2)
+    feladat_eldontes(Vonat_nevek, Indulasok_1, Indulasok_2, Orszag_berlet)
     gyakorisag_mero(Vonat_nevek)
-    # print(megszamolas(Orszag_berlet))
-    listazas(Vonat_nevek, Indulasok_1, Indulasok_2)
+    print(megszamolas(Orszag_berlet))
+    listazas(Vonat_nevek, Indulasok_1, Indulasok_2, Orszag_berlet)
 
 
 main()
