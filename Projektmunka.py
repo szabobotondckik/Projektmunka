9f0bb6481bf898d20394b1011945febf6e3a95ff
szabobotondckik <szabo.botond@ckik.hu>
2024-04-24 18:57:04 +0200

    Fájl beírás megoldás hogy most valemnnyire műkszik gyakoriság mérő újra cisnálása de kell majd rjata szerkeszteni összegzés benne van de borzalmas listázás újra csinálása

--- Projektmunka.py
+++ Projektmunka.py
@@ -1,10 +1,11 @@
 from random import *
+import webbrowser
 
 
 def feladat_eldontes(Vonat_nevek, Indulasok_1, Indulasok_2, Orszag_berlet):
         feladat = input("Mi legyen a program feladata: ")
         melyik_fajl = input("Melyik fájlt dolgozzuk fel: ")
-        if feladat == "Olvasas" or "olvasas":
+        if feladat == "Olvasas" or  feladat == "olvasas":
                 if melyik_fajl == "Be1.txt":
                     befajl_1(Vonat_nevek, Indulasok_1, Indulasok_2, Orszag_berlet)
                 elif melyik_fajl == "Be2.txt":
@@ -12,56 +13,83 @@ def feladat_eldontes(Vonat_nevek, Indulasok_1, Indulasok_2, Orszag_berlet):
                 else:    
                     print("Nem jó adatokat adtál meg légyszi újra írd be :3")
                     feladat_eldontes(Vonat_nevek, Indulasok_1, Indulasok_2, Orszag_berlet)
-        elif feladat == "Iras" or "iras":
+        elif feladat == "Iras" or feladat == "iras":
                 if melyik_fajl == "Be1.txt":
                     iras_fajl_1(Vonat_nevek, Indulasok_1, Indulasok_2, Orszag_berlet)
                 elif melyik_fajl == "Be2.txt":
                     iras_fajl_2(Vonat_nevek, Indulasok_1, Indulasok_2, Orszag_berlet)
                 else:
                     print("Nem jó adatokat adtál meg légyszi újra írd be :3")
-                    feladat_eldontes(Vonat_nevek, Indulasok_1, Indulasok_2, Orszag_berlet)        
+                    feladat_eldontes(Vonat_nevek, Indulasok_1, Indulasok_2, Orszag_berlet)
+        elif feladat == "Kuhar" or feladat == "kuhar":
+            webbrowser.open_new("https://www.youtube.com/watch?v=gDBCG1e-qPY")
+            feladat_eldontes(Vonat_nevek, Indulasok_1, Indulasok_2, Orszag_berlet)
+        else:
+             print("Nem jó adatokat adtál meg légyszi újra írd be :3")
+             feladat_eldontes(Vonat_nevek, Indulasok_1, Indulasok_2, Orszag_berlet)  
+
     
         
 
 def iras_fajl_1(Vonat_nevek, Indulasok_1, Indulasok_2, Orszag_berlet):
-    Menetrend_iro = open("Be1.txt", "a", encoding="UTF-8") 
-    hozza_adott_sor = input("Mit szertenél hozzá írni (szabályokat tarts meg): ")
-    Menetrend_iro.write(f"\n{hozza_adott_sor}")
-    Menetrend_iro = open("Be1.txt", "r", encoding="UTF-8")
-    for sor in Menetrend_iro.readlines():
-        m = sor.split("-")
-        Vonat_nevek.append(m[0])
-        Indulasok_1.append(m[int(1)].strip("\n"))
-        Indulasok_2.append(m[int(2)].strip("\n"))
-        Orszag_berlet.append(m[(3)-1].strip("\n"))
-    # fajl_torles()
-    print(Vonat_nevek)
-    Menetrend_iro.close()
+    try:
+        hozza_adott_sor = input("Mit szertenél hozzá írni (szabályokat tarts meg): ")
+        Menetrend_iro = open("Be1.txt", "a", encoding="UTF-8") 
+        Menetrend_iro.write(f"\n{hozza_adott_sor}")
+        Menetrend_iro = open("Be1.txt", "r", encoding="UTF-8")
+        for sor in Menetrend_iro.readlines():
+            m = sor.split("-")
+            Vonat_nevek.append(m[0])
+            Indulasok_1.append(m[int(1)].strip("\n"))
+            Indulasok_2.append(m[int(2)].strip("\n"))
+            Orszag_berlet.append(m[(3)-1].strip("\n"))
+        print(Vonat_nevek)
+        Menetrend_iro.close()
+    except:
+            print("Légyszíves próbáld újra minta szerint")
+            print("Minta: Z50 - 5 - 18 - Utazható Országbérlettel")
+            Menetrend_iro = open("Be1.txt", "r", encoding="UTF-8") 
+            for sor in Menetrend_iro.readlines():
+                if sor == 62:
+                    Menetrend_iro = open("Be1.txt", "w", encoding="UTF-8")  
+                    Menetrend_iro.writelines([" "])
+                    Menetrend_iro.close()
+            iras_fajl_1(Vonat_nevek, Indulasok_1, Indulasok_2, Orszag_berlet)
+    
 
 def iras_fajl_2(Vonat_nevek, Indulasok_1, Indulasok_2, Orszag_berlet):
-    Menetrend_iro = open("Be2.txt", "a", encoding="UTF-8")
-    hozza_adott_sor = input("Mit szertenél hozzá írni (szabályokat tarts meg): ")
-    Menetrend_iro.write(f"\n{hozza_adott_sor}")
-    Menetrend_iro = open("Be2.txt", "r", encoding="UTF-8")
-    for sor in Menetrend_iro.readlines():
-        m = sor.split("-")
-        Vonat_nevek.append(m[0])
-        Indulasok_1.append(m[int(1)].strip("\n"))
-        Indulasok_2.append(m[int(2)].strip("\n"))
-        Orszag_berlet.append(m[(3)-1].strip("\n"))
-    print(Vonat_nevek)
-    Menetrend_iro.close()
+    try:
+        hozza_adott_sor = input("Mit szertenél hozzá írni (szabályokat tarts meg): ")
+        Menetrend_iro = open("Be2.txt", "a", encoding="UTF-8") 
+        Menetrend_iro.write(f"\n{hozza_adott_sor}")
+        Menetrend_iro = open("Be2.txt", "r", encoding="UTF-8")
+        for sor in Menetrend_iro.readlines():
+            m = sor.split("-")
+            Vonat_nevek.append(m[0])
+            Indulasok_1.append(m[int(1)].strip("\n"))
+            Indulasok_2.append(m[int(2)].strip("\n"))
+            Orszag_berlet.append(m[(3)-1].strip("\n"))
+        print(Vonat_nevek)
+        Menetrend_iro.close()
+    except:
+            print("Légyszíves próbáld újra minta szerint")
+            print("Minta: Z50 - 5 - 18 - Utazható Országbérlettel")
+            Menetrend_iro = open("Be2.txt", "r", encoding="UTF-8") 
+            for sor in Menetrend_iro.readlines():
+                if sor == 28:
+                    Menetrend_iro = open("Be2.txt", "w", encoding="UTF-8")  
+                    Menetrend_iro.writelines([" "])
+                    Menetrend_iro.close()
+            iras_fajl_2(Vonat_nevek, Indulasok_1, Indulasok_2, Orszag_berlet)
 
 def befajl_1(Vonat_nevek, Indulasok_1, Indulasok_2, Orszag_berlet):
     Menetrend_beolvaso = open("Be1.txt", "r", encoding="UTF-8")
     for sor in Menetrend_beolvaso.readlines():
         m = sor.split("-")
         Vonat_nevek.append(m[0])
-        Indulasok_1.append(m[int(1)])
-        Indulasok_2.append(m[int(2)])
+        Indulasok_1.append(m[(1)].strip()) 
+        Indulasok_2.append(m[(2)].strip()) 
         Orszag_berlet.append(m[(3)].strip("\n"))
-    # print(Vonat_nevek)
-    # print(Indulasok)
     Menetrend_beolvaso.close()
 
 
@@ -80,33 +108,60 @@ def befajl_2(Vonat_nevek, Indulasok_1, Indulasok_2, Orszag_berlet):
 
 
 def listazas(Vonat_nevek, Indulasok_1, Indulasok_2, Orszag_berlet):
-    print("Vonatok Nevei:")
-    for i in range(len(Vonat_nevek)):
-        print(Vonat_nevek[i], end=" ")
+    print("Hogy szertenéd hogy ki írassuk az adatokat?")
+    print("1 Txt szerint 1 sorban 4 adat?")
+    print("2 Vagy minden adatot külön lista szerűen?")
+    kiiras = input("")
     print("\n")
-    print("Vonat Órai indulása:")
-    for i in range(len(Indulasok_1)):
-        print(Indulasok_1[i], end=" ")
-    print()
-    print("Vonat Perci indulása:")
-    for i in range(len(Indulasok_2)):
-        print(Indulasok_2[i], end=" ")
-    print()
-    print("Vonat országbérlet lehetőségei: ")
-    for i in range(len(Orszag_berlet)):
-        print(f"{Orszag_berlet[i]}\n")
-    print()
+    if kiiras == "1":
+        print("Vonatok Nevei:")
+        for i in range(len(Vonat_nevek)):
+            print(Vonat_nevek[i], end=" ")
+        print("\n")
+        print("Vonat Órai indulása:")
+        for i in range(len(Indulasok_1)):
+            print(Indulasok_1[i], end=" ")
+        print("\n")
+        print("Vonat Perci indulása:")
+        for i in range(len(Indulasok_2)):
+            print(Indulasok_2[i], end=" ")
+        print("\n")
+        print("Vonat országbérlet lehetőségei: ")
+        for i in range(len(Orszag_berlet)):
+            print(f"{Orszag_berlet[i]}\n")
+        print("\n")
+    elif kiiras == "2":
+        for i in range(len(Vonat_nevek)):
+            print(Vonat_nevek[i] + Indulasok_1[i] + Indulasok_2[i] + Orszag_berlet[i])
+            print("")
  
 
 
 
-def gyakorisag_mero(Vonat_nevek):
+def Indulas_mero(Indulasok_1):
     maxi = 0
-    for i in range(1, len(Vonat_nevek)):
-        if Vonat_nevek[i] > Vonat_nevek[maxi]:
+    for i in range(1, len(Indulasok_1)):
+        if Indulasok_1[i] > Indulasok_1[maxi]:
             maxi = i
             maxi += 1
-    print(f"A leggyakoribb vonat: {Vonat_nevek[i]}")
+    print(f"A leg későbbi indulás{Indulasok_1[i]}")
+    
+def gyakorisag_mero(Vonat_nevek):
+    Gyakorisag = {}
+    for darab in Vonat_nevek:
+        if darab in Gyakorisag:
+                Gyakorisag[darab] += 1
+        else:
+            Gyakorisag[darab] = 1
+    leggyakoribb = None
+    maxi = 0
+    for darab, megszamitas in Gyakorisag.items():
+        if  megszamitas > maxi:
+            maxi = megszamitas
+            leggyakoribb = darab
+    
+    return leggyakoribb
+
 
 def megszamolas(Orszag_berlet):
     db = 0
@@ -118,14 +173,18 @@ def megszamolas(Orszag_berlet):
 
 def kivalogatas(Orszag_berlet, Vonat_nevek, Utazhato):
     n = len(Orszag_berlet)
+    j = 0
     for i in range(n):
         if Orszag_berlet[i] == " Utazható Országbérlettel":
-            if Vonat_nevek[i] not in Utazhato:  
+            if Vonat_nevek[i] not in Utazhato: 
                 Utazhato.append(Vonat_nevek[i])
-    print(f"{', '.join(Utazhato)}\n")
+    for j in range(len(Utazhato)):
+        print(Utazhato[j], end=" ")
+    print("")
+        
     
 
-def kereses(Indulasok_1, Indulasok_2, Vonat_nevek, r):
+def kereses(Indulasok_1, Vonat_nevek, r):
     n = len(Vonat_nevek)
     i = 0
     print(r)
@@ -134,19 +193,20 @@ def kereses(Indulasok_1, Indulasok_2, Vonat_nevek, r):
     if i < n:
         return Vonat_nevek[i]
             
+            
+def osszegzes(Indulasok_1, Indulasok_2):
+    s = 0
+    for i in range(0, len(Indulasok_1)-1):
+        if Indulasok_1[i+1] == Indulasok_1[i] or Indulasok_1[i-1] == Indulasok_1[i] or Indulasok_1[i-1] == Indulasok_1[i+1]:
+            Ido = int(Indulasok_2[i]) + int(Indulasok_2[i-1])
+            s += Ido
+        else:
+            Ido = int(Indulasok_2[i])
+            s += Ido
+    return s
 
     
     
-    
-# def fajl_torles():
-#     fajl_eredetibe_allitasa = input("Ki akarod azt a fájlt törölni amit bele írtál?: ")
-#     if fajl_eredetibe_allitasa == "Igen" or "igen":
-#         melyik_txt = input("Melyik txt-t használtad?: ")
-#         if melyik_txt == "Be1.txt":
-#             Menetrend_beolvaso = open("Be1.txt", "w+", encoding="UTF-8")
-#             Menetrend_beolvaso.seek(62)
-#             Menetrend_beolvaso.writelines("")
-#             Menetrend_beolvaso.close()
 
             
 
@@ -170,13 +230,14 @@ def main():
     Indulasok_2 = []
     Orszag_berlet = []
     feladat_eldontes(Vonat_nevek, Indulasok_1, Indulasok_2, Orszag_berlet)
-    gyakorisag_mero(Vonat_nevek)
+    Indulas_mero(Indulasok_1)
     print(f"Országbérlettel utazható vonatok száma: {megszamolas(Orszag_berlet)}")
     kivalogatas(Orszag_berlet, Vonat_nevek, Utazhato)
+    print(f"A leggyakoribb vonat: {gyakorisag_mero(Vonat_nevek)}")
     rendez(Vonat_nevek, Indulasok_1, Indulasok_2, Orszag_berlet)
     listazas(Vonat_nevek, Indulasok_1, Indulasok_2, Orszag_berlet)
-    print(kereses(Vonat_nevek, Indulasok_1, Indulasok_2, r))
-    # fajl_torles()
+    print(osszegzes(Indulasok_1, Indulasok_2))
+    print(kereses(Indulasok_1, Vonat_nevek, r))
 
 main()
 
